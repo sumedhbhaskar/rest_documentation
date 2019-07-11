6bfbe926318a85df7336bcf234acf7917522aa0a
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2019-07-11 14:45:41 +0530

    added login, permissions

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include
+
 
 urlpatterns=[
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view() ),
+    path('users/<int:pk>', views.UserDetail.as_view()),
+    
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -8,13 +10,29 @@ class SnippetList(generics.ListCreateAPIView):
     list all the snippets and create new snippet
 
     """
-    querysets = Snippet.objects.all()
+    queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    permission_classes= (permissions.IsAuthenticatedOrReadOnly,)  
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     """
     Now this contain detailed view of a snippet, it can be updated, deleted or retrived
     """
-    querysets = Snippet.objects.all()
+    queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes= (permissions.IsAuthenticatedOrReadOnly,)  
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer        
